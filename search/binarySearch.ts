4d2918dafb912c7ef58af6b957481d03de4a4d63
huyf <huyf@thingword.com>
2023-11-27 11:22:33 +0800

    优化二分查找写法

--- search/binarySearch.ts
+++ search/binarySearch.ts
@@ -1,22 +1,24 @@
 /**
  * 二分查找
  * @param key 要查找的值
- * @param array 一个顺序的数组
+ * @param array 一个有序的数组
  * @returns 要查找的值在数组中的索引
  */
+export default function binarySearch<T>(key: T, array: T[]): number {
+  let start: number = 0
+  let end: number = array.length - 1
+  let target: number = -1
 
-export default function binarySearch<T>(key:T, array:T[]):number {
-  let start:number = 0
-  let end:number = array.length - 1
   while (start < end) {
-    let mid:number = start + Math.floor((end - start) / 2)
+    let mid: number = start + Math.floor((end - start) / 2)
     if (key < array[mid]) {
       end = mid - 1
     } else if (key > array[mid]) {
       start = mid + 1
     } else {
-      return mid
+      target = mid
     }
   }
-  return -1
+
+  return target
 }
