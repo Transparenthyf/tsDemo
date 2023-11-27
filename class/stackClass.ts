be3838caf1e49c277099a6f9dffb4f3c44de7891
Transparenthyf <18098581870@163.com>
2023-11-27 14:11:21 +0800

    基于数组实现栈类

--- class/stackClass.ts
+++ class/stackClass.ts
@@ -1,37 +1,50 @@
 export default class Stack<T> {
-  private array:T[]
-  private N:number
+  private array: T[]
 
   constructor() {
     this.array = []
-    this.N = 0
   }
 
   /**
    * 添加一个元素
    */
-  public push(item:T):void {
-    this.array[this.N++] = item
+  public push(item: T): void {
+    this.array.push(item)
   }
 
   /**
    * 删除最早添加的元素
    */
-  public pop():T {
-    return this.array[--this.N]
+  public pop(): T {
+    if (this.isEmpty()) {
+      throw new Error('栈为空')
+    } else {
+      return this.array.pop()
+    }
   }
 
   /**
    * 队列是否为空
    */
-  public isEmpty():boolean {
-    return this.N === 0
+  public isEmpty(): boolean {
+    return this.array.length === 0
   }
 
   /**
    * 队列中的元素数量
    */
-  public size():number {
-    return this.N
+  public size(): number {
+    return this.array.length
+  }
+
+  /**
+   * 访问栈顶元素
+   */
+  public peek(): T {
+    if (this.isEmpty()) {
+      throw new Error('栈为空')
+    } else {
+      return this.array[this.array.length - 1]
+    }
   }
 }
