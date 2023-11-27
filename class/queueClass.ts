fce77132bfd3baded894f48eebc5cb2fef581b59
Transparenthyf <18098581870@163.com>
2023-11-27 14:31:22 +0800

    基于数组实现队列

--- class/queueClass.ts
+++ class/queueClass.ts
@@ -1,38 +1,50 @@
 export default class Queue<T> {
-  private array:T[]
-  private N:number
+  private array: T[]
 
   constructor() {
     this.array = []
-    this.N = 0
   }
 
   /**
    * 添加一个元素
    */
-  public enqueue(item:T):void {
-    this.array[this.N++] = item
+  public enqueue(item: T): void {
+    this.array.push(item)
   }
 
   /**
    * 删除最早添加的元素
    */
-  public dequeue():T {
-    this.N--
-    return this.array.splice(0, 1)[0]
+  public dequeue(): T {
+    if (this.isEmpty()) {
+      throw new Error('队列为空')
+    } else {
+      return this.array.shift()
+    }
+  }
+
+  /**
+   * 访问队首元素
+   */
+  public peek(): T {
+    if (this.isEmpty()) {
+      throw new Error('队列为空')
+    } else {
+      return this.array[0]
+    }
   }
 
   /**
    * 队列是否为空
    */
-  public isEmpty():boolean {
-    return this.N === 0
+  public isEmpty(): boolean {
+    return this.array.length === 0
   }
 
   /**
    * 队列中的元素数量
    */
-  public size():number {
-    return this.N
+  public size(): number {
+    return this.array.length
   }
 }
