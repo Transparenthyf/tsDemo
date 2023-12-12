eae96e1edbf7398fa077a288d145b60c808d01bb
Transparenthyf <18098581870@163.com>
2023-12-12 13:59:18 +0800

    二叉树节点类增加父节点属性

--- class/binaryTreeNodeClass.ts
+++ class/binaryTreeNodeClass.ts
@@ -1,11 +1,22 @@
 /** 二叉树节点类 */
 export default class BinaryTreeNode<T> {
+  /** 节点值 */
   val: T
+  /** 父节点 */
+  parentNode: BinaryTreeNode<T> | null
+  /** 左节点 */
   leftNode: BinaryTreeNode<T> | null
+  /** 右节点 */
   rightNode: BinaryTreeNode<T> | null
 
-  constructor(val: T, leftNode?: BinaryTreeNode<T> | null, rightNode?: BinaryTreeNode<T> | null) {
+  constructor(
+    val: T,
+    parentNode?: BinaryTreeNode<T> | null,
+    leftNode?: BinaryTreeNode<T> | null,
+    rightNode?: BinaryTreeNode<T> | null
+  ) {
     this.val = val
+    this.parentNode = parentNode === undefined ? null : parentNode
     this.leftNode = leftNode === undefined ? null : leftNode
     this.rightNode = rightNode === undefined ? null : rightNode
   }
