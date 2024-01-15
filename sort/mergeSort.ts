dbbd96338d1f1298f8a4ad37961b29559ae06894
Transparenthyf <18098581870@163.com>
2024-01-15 15:06:47 +0800

    修复排序错乱的问题

--- sort/mergeSort.ts
+++ sort/mergeSort.ts
@@ -15,6 +15,10 @@ export default function mergeSort<T>(array: T[], start?: number, end?: number): 
 
   /** 中间位置索引 */
   let mid: number = Math.floor((start + end) / 2)
+
+  mergeSort(array, start, mid)
+  mergeSort(array, ++mid, end)
+
   // mid 将 start 到 end 这段数组分为左右两段
   /** 左侧数组起始位置索引 */
   let left: number = start
@@ -22,9 +26,6 @@ export default function mergeSort<T>(array: T[], start?: number, end?: number): 
   let right: number = mid
   /** 临时储存排序后的数组 */
   let tmpArray: T[] = []
-
-  mergeSort(array, start, mid)
-  mergeSort(array, ++mid, end)
 
   // 将左右两侧数组整合
   for (let i = start; i <= end; i++) {
--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
-import quickSort from './sort/quickSort'
+import mergeSort from './sort/mergeSort'
 
 let array = [9, 2, 3, 5, 7, 3, 5, 8, 2, 9, 4, 3, 2, 7]
 
-console.log(quickSort(array))
+console.log(mergeSort(array))
